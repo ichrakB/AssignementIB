b77f323212470c86cc309951c7d55e782c1f972d
ichrakB <ichrakborgi@yahoo.fr>
2021-10-01 17:13:06 +0100

    Create Project

---
+++ IBAssignmnt/IBAssignmntTests/IBAssignmntTests.swift
@@ -0,0 +1,33 @@
+//
+//  IBAssignmntTests.swift
+//  IBAssignmntTests
+//
+//  Created by Ichrak BORGI on 1/10/2021.
+//
+
+import XCTest
+@testable import IBAssignmnt
+
+class IBAssignmntTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
